9d09376c560e890ffc2477c5851b0c477152e83b
julien <julienchauvin@hotmail.fr>
2025-07-25 13:19:24 +0200

    création des entité sweatshirt et taille

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\SweatshirtRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,10 +10,13 @@ use Symfony\Component\Routing\Attribute\Route;
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'home')]
-    public function index(): Response
+    public function index(SweatshirtRepository $sweatshirtRepository): Response
     {
+
+        $sweatshirtsTop = $sweatshirtRepository->findTop();
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'sweatshirtsTop' => $sweatshirtsTop
         ]);
     }
 }
---
+++ src/Entity/Sweatshirt.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\SweatshirtRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: SweatshirtRepository::class)]
+class Sweatshirt
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?float $price = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?bool $top = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function isTop(): ?bool
+    {
+        return $this->top;
+    }
+
+    public function setTop(?bool $top): static
+    {
+        $this->top = $top;
+
+        return $this;
+    }
+}
